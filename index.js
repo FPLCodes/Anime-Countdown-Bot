7dcf79fbbdd63fd239dcde253cd9a5b37d032e61
Sahab <sahabferdous1@gmail.com>
2022-06-15 16:35:38 -0400

    Changed TOKEN

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const Discord = require("discord.js");
 require("dotenv").config();
 
-const TOKEN =
-  "ODU0MjIwMTg3MTAwNTEyMjY2.G1Bdr-.7OkGGxo6G9Eh4tlc5lVMrxUE5Z1PD4tDpnc2rY";
-
 const client = new Discord.Client({
   intents: ["GUILDS", "GUILD_MESSAGES"],
 });
