c24d3129018858369b72bcd8d56923e41c0e89c8
Sahab <sahabferdous1@gmail.com>
2022-06-15 17:26:20 -0400

    Created handlers, events, commands

--- index.js
+++ index.js
@@ -5,14 +5,22 @@ const client = new Discord.Client({
   intents: ["GUILDS", "GUILD_MESSAGES"],
 });
 
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}`);
-});
+let bot = {
+  client,
+  prefix: "cd",
+  owners: ["274509924371922946"],
+};
 
-client.on("messageCreate", (message) => {
-  if (message.content == "hi") {
-    message.reply("Hello world!");
-  }
-});
+client.commands = new Discord.Collection();
+client.events = new Discord.Collection();
+
+client.loadEvents = (bot, reload) => require("./handlers/events")(bot, reload);
+client.loadCommands = (bot, reload) =>
+  require("./handlers/commands")(bot, reload);
+
+client.loadEvents(bot, false);
+client.loadCommands(bot, false);
+
+module.exports = bot;
 
 client.login(process.env.TOKEN);
