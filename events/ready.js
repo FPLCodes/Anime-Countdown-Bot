9f1c4777b1e912f48865c0d55723ba8e71826d17
Sahab <sahabferdous1@gmail.com>
2022-06-16 16:26:17 -0400

    Changed bot status

--- events/ready.js
+++ events/ready.js
@@ -2,5 +2,8 @@ module.exports = {
   name: "ready",
   run: async (bot) => {
     console.log("Logged in as " + bot.client.user.tag);
+    bot.client.user.setActivity(`anime with onii-chan`, {
+      type: "WATCHING",
+    });
   },
 };
