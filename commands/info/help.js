039551c3b20661104b95b3627423f4f767209cce
Sahab <sahabferdous1@gmail.com>
2022-06-16 23:37:21 -0400

    Added try catch to help command

--- commands/info/help.js
+++ commands/info/help.js
@@ -6,15 +6,19 @@ module.exports = {
   permissions: [],
   devOnly: false,
   run: async ({ client, message, args }) => {
-    const embed = new MessageEmbed()
-      .setColor("#808080")
-      .setTitle("INSTRUCTIONS")
-      .setDescription(
-        "After the prefix `ani`, type `cd` followed by the anime title"
-      )
-      .addField("Example", "`ani cd spy x family`")
-      .setTimestamp();
+    try {
+      const embed = new MessageEmbed()
+        .setColor("#808080")
+        .setTitle("INSTRUCTIONS")
+        .setDescription(
+          "After the prefix `ani`, type `cd` followed by the anime title"
+        )
+        .addField("Example", "`ani cd spy x family`")
+        .setTimestamp();
 
-    message.reply({ embeds: [embed] });
+      message.reply({ embeds: [embed] });
+    } catch (error) {
+      message.reply("Sorry there was an error");
+    }
   },
 };
